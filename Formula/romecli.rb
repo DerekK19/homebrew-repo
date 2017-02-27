214d39890decd3239cba528f9437ac8ba69109ac
Derek Knight <derek.knight@orionhealth.com>
2017-02-28 10:45:24 +1300

    RomeCLI formula changed to have correct SHA256 and urls

--- Formula/romecli.rb
+++ Formula/romecli.rb
@@ -1,8 +1,8 @@
 class Romecli < Formula
   desc "CLI tool for Rome Server written in Swift"
-  homepage "https://github.com/146BC/RomeCLI"
-  url "https://github.com/146BC/RomeCLI/archive/0.2.1.tar.gz"
-  sha256 "246f3ed89c9a90062cfdd3d8f1d71232ab93e7e207b7499034b044c69be4e480"
+  homepage "https://github.com/DerekK19/RomeCLI"
+  url "https://github.com/DerekK19/RomeCLI/archive/0.3.0.tar.gz"
+  sha256 "70ef7ae65cb604bc107db967bb769dd9e01f3041881a5617113ae2460b0bb6a9"
 
   depends_on :xcode => ["7.3", :build]
   depends_on "carthage"
